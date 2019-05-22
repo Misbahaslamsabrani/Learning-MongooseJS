381a86552da5f0814ea5c26ea9af2f128fe9d84d
misbahaslamsabrani <misbahaslamsabrani@gmail.com>
2019-05-22 15:37:42 +0500

    Express Server (GET/POST)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 require("./db/mongoose");
 const Profiles = require("./models/profiles");
+const express = require("express");
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
 
-const newRec = Profiles({
+
+/* const newRec = Profiles({
     name: "Misbah",
     age: 23,
     graduate: false,
     email: "misbah@gmail.com"
 })
 
-newRec.save().then(result => console.log(result)).catch(e => console.log(e))
+newRec.save().then(result => console.log(result)).catch(e => console.log(e)) */
+
+
+app.post("/profiles", (req, res) => {
+    const profile = Profiles(req.body)
+    profile.save().then(() => {
+        res.send(profile)
+    }).catch((e) => {
+        res.status(400)
+        res.send(e)
+    })
+})
+
+app.get("/profiles/:id", (req, res) => {
+    const _id = req.params.id
+    console.log(_id)
+    Profiles.findById(_id).then((user) => {
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+app.listen(port, () => {
+    console.log("Server is up on port " + port)
+})
